9565939c7cda78e414b2eea868a134c62fda9b5a
Gustavo Mata <dev.gustta.h@gmail.com>
2023-06-03 20:32:17 -0300

    feat(clock-components): export clock components

---
+++ components/clock/components/index.tsx
@@ -0,0 +1,4 @@
+export { ClockColorPicker } from './ClockColorPicker'
+export { ClockItalic } from './ClockItalic'
+export { ClockBold } from './ClockBold'
+export { ClockFontSize } from './ClockFontSize'
