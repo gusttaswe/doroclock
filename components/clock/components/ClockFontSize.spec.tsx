cab7d8be3e9934f2e3c1b268e0f043bb8cce0d0e
Gustavo Mata <dev.gustta.h@gmail.com>
2023-06-03 20:31:35 -0300

    test(ClockFontSize): add unit tests

---
+++ components/clock/components/ClockFontSize.spec.tsx
@@ -0,0 +1,51 @@
+import { fireEvent, render, waitFor } from "@testing-library/react";
+import { ClockFontSize } from "./ClockFontSize";
+import { debug } from "console";
+
+describe('ClockBold Component', () => {
+  it('Should render the Button Correctly', () => {
+    const { getByTestId } = render(<ClockFontSize fontSize={1} updateFontSize={() => (1)} />);
+    const clockFontsizeElement = getByTestId('clock-fontsize');
+    expect(clockFontsizeElement).toBeInTheDocument();
+  })
+  
+  it('Should NOT decrement fontSize if it is already at the minimum value of 1', async () => {
+    const updateFontSizeMock = jest.fn();
+    const { getByText } = render(<ClockFontSize fontSize={1} updateFontSize={updateFontSizeMock} />);
+
+    const decrementFontSizeButton = getByText('-');
+    fireEvent.click(decrementFontSizeButton);
+
+    expect(updateFontSizeMock).not.toHaveBeenCalled();
+  })
+
+  it('Should call the updateFontSize function when decrement button is clicked', async () => {
+    const updateFontSizeMock = jest.fn();
+    const { getByText } = render(<ClockFontSize fontSize={4} updateFontSize={updateFontSizeMock} />);
+
+    const decrementFontSizeButton = getByText('-');
+    fireEvent.click(decrementFontSizeButton);
+
+    expect(updateFontSizeMock).toHaveBeenCalledWith(3);
+  })
+
+  it('Should call the updateFontSize function when increment button is clicked', async () => {
+    const updateFontSizeMock = jest.fn();
+    const { getByText } = render(<ClockFontSize fontSize={4} updateFontSize={updateFontSizeMock} />);
+
+    const incrementFontSizeButton = getByText('+');
+    fireEvent.click(incrementFontSizeButton);
+
+    expect(updateFontSizeMock).toHaveBeenCalledWith(5);
+  })
+ 
+  it('Should call the updateFontSize function when increment button is clicked', async () => {
+    const updateFontSizeMock = jest.fn();
+    const { getByTestId } = render(<ClockFontSize fontSize={4} updateFontSize={updateFontSizeMock} />);
+
+    const fontSizeInput = getByTestId('clock-fontsize-input');
+    fireEvent.change(fontSizeInput, { target: { value: 10 } });
+
+    expect(updateFontSizeMock).toHaveBeenCalledWith(10);
+  })
+})
---
+++ components/clock/components/ClockFontSize.tsx
@@ -0,0 +1,37 @@
+
+type ClockFontSize = {
+  fontSize: number;
+  updateFontSize(newSize: number): void;
+}
+
+export function ClockFontSize({
+  fontSize,
+  updateFontSize
+}: ClockFontSize) {
+
+  const handleFontSizeChange = (fontSize: number) => {
+    if (fontSize < 1) return;
+    updateFontSize(fontSize);
+  };
+
+  return (
+    <div className="flex gap-2" data-testid='clock-fontsize'>
+      <button 
+        className="w-10 font-bold rounded-md" 
+        onClick={() => handleFontSizeChange(fontSize - 1)}
+      > - </button>
+      <input 
+        data-testid='clock-fontsize-input'
+        className="w-10 border border-slate-500 rounded-md text-center"
+        type="text" 
+        value={fontSize}
+        min={1}
+        onChange={(e) => handleFontSizeChange(Number(e.target.value))} 
+      />
+      <button 
+        className="w-10 font-bold rounded-md"
+        onClick={() => handleFontSizeChange(fontSize + 1)}
+      >+</button>
+    </div>
+  )
+}
