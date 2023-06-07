f5a8f177ebb2428aac81cc7fa2c9c541f41da33f
Gustavo Mata <dev.gustta.h@gmail.com>
2023-06-07 16:56:33 -0300

    feat(fonts): add inter font

--- app/fonts/press-start-2p.tsx
+++ app/fonts/press-start-2p.tsx
@@ -1,6 +1,6 @@
 import { Press_Start_2P } from "next/font/google";
 
-export const pressStart2P = Press_Start_2P({
+export const pressStart2p = Press_Start_2P({
   subsets: ['latin'],
   variable: '--font-press-start_2p',
   display: 'swap',
