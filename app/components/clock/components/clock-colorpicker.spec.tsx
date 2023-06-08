35ae88ecdd776fabada779cc3e27fdba68ec769e
Gustavo Mata <dev.gustta.h@gmail.com>
2023-06-08 19:02:01 -0300

    test(clock-colorpicker): add unit tests

--- app/components/clock/components/clock-colorpicker.spec.tsx
+++ app/components/clock/components/clock-colorpicker.spec.tsx
@@ -2,28 +2,45 @@ import { render, fireEvent, waitFor } from '@testing-library/react';
 import { ClockColorPicker } from './clock-colorpicker';
 
 describe('ClockColorPicker Component', () => {
+  const customColor = ['#D9E3F0', '#F47373', '#697689'];
+  const updateColorMock = jest.fn();
 
-  it('should render the component', () => {
-    const { getByTestId } = render(<ClockColorPicker currentColor="#000" updateColor={() => {}} />);
+  it('Should render the component correctly without a image', () => {
+    const { getByTestId } = render(
+      <ClockColorPicker 
+        currentColor='#FFFFFF'
+        currentImage=''
+        customColors={customColor}
+        updateColor={updateColorMock}
+      />
+    );
+
     const colorElement = getByTestId('clock-color-picker');
+    const pickColorButton = getByTestId('pick-color-button');
     expect(colorElement).toBeInTheDocument();
+    expect(pickColorButton).toBeInTheDocument();
+
+    const customColorContainer = getByTestId('color-options-container');
+    const colorButtonElements = customColorContainer.getElementsByTagName('button');
+    expect(colorButtonElements.length).toEqual(customColor.length);
   });
 
   it('should call the updateColor function with the selected color', async () => {
-    const updateColorMock = jest.fn();
-    const customColors = ['#000000', '#888000', '#cecece'];
-
-    const { findByTitle } = render(
-      <ClockColorPicker currentColor="#000" updateColor={updateColorMock} customColors={customColors} />
+    const { getByTestId } = render(
+      <ClockColorPicker 
+        currentColor='#FFFFFF'
+        currentImage=''
+        customColors={customColor}
+        updateColor={updateColorMock}
+      />
     );
 
-    const colorPickerElement = await findByTitle(customColors[1]);
+    const colorPickerContainer = await getByTestId('color-options-container');
+    const colorButtonElement = colorPickerContainer.getElementsByTagName('button');
 
-    fireEvent.click(colorPickerElement);
+    fireEvent.click(colorButtonElement[1]);
 
-    const newColor = customColors[1];
-    await waitFor(() => {
-      expect(updateColorMock).toHaveBeenCalledWith(newColor);
-    })
+    const newColor = customColor[1];
+    expect(updateColorMock).toHaveBeenCalledWith(newColor);
   });
 });
--- app/components/clock/components/clock-colorpicker.tsx
+++ app/components/clock/components/clock-colorpicker.tsx
@@ -1,26 +1,75 @@
-import { TwitterPicker } from "react-color";
+import { Pipette } from 'lucide-react'
+import { createPortal } from 'react-dom';
+import ImageColorPicker from '../../image-color-picker';
+import { useState } from 'react';
 
 type ClockColorPicker = {
   currentColor: string;
   updateColor(color: string): void;
-  customColors?: string[];
+  customColors: string[];
+  currentImage: string;
+}
+
+function isLightColor(hexColor: string) {
+  const [red, green, blue] = hexColor.replace('#', '').match(/.{2}/g)!.map(component => parseInt(component, 16));
+  const luminance = (0.2126 * red + 0.7152 * green + 0.0722 * blue) / 255;
+  return luminance > 0.5;
 }
 
 export function ClockColorPicker({
   currentColor,
   updateColor,
-  customColors
+  customColors,
+  currentImage
 }: ClockColorPicker) {
+  const [isImageSelectorOpen, setIsImageColorPickOpen] = useState(false);
+  const color = currentColor.replace('#', "");
+  const isLight = isLightColor(color);
+  const pickerTheme = isLight ? 'black' : 'white';
 
   return (
-    <div id="color-settings" data-testid="clock-color-picker">
-      <TwitterPicker
-        colors={customColors}
-        className="mt-4"
-        triangle="hide"
-        color={currentColor}
-        onChangeComplete={({ hex }) => updateColor(hex)}
-      />
-    </div>
+    <>
+      <div 
+        id="color-settings" 
+        data-testid="clock-color-picker"
+        className="flex justify-center gap-2 flex-wrap"
+      >
+        <div>
+          <button 
+            data-testid="pick-color-button"
+            className={`w-8 h-8 flex justify-center items-center rounded-md`}
+            style={{
+              background: currentColor,
+              border: `1px solid ${pickerTheme}`
+            }}
+            onClick={() => setIsImageColorPickOpen(prev => !prev)}
+          >
+            <Pipette size={15} color={pickerTheme} />
+          </button>
+        </div>
+        <div data-testid="color-options-container">
+          { customColors.map((color) => (
+            <button 
+              key={color} 
+              className="w-8 h-8 rounded-md" 
+              style={{ backgroundColor: color }} 
+              onClick={() => updateColor(color)}
+            />
+            )) 
+          }
+        </div>
+      </div>
+
+      { (isImageSelectorOpen && currentImage) && createPortal(
+        <ImageColorPicker 
+          imageBlob={currentImage} 
+          updateColor={(newColor) => {
+            updateColor(newColor)
+            setIsImageColorPickOpen(false)
+          }}  
+        />,
+        document.body
+      )}
+    </>
   )
 }
