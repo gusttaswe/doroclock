2744415cd2b8dae9a0d546bb2722b94404445749
Gustavo Mata <dev.gustta.h@gmail.com>
2023-06-09 08:55:14 -0300

    refactor(footer): remove modal

--- app/components/home/footer/index.tsx
+++ app/components/home/footer/index.tsx
@@ -1,8 +1,5 @@
 'use client'
-import { FooterModal } from "@/app/components/modal"
-import { ChevronUp } from "lucide-react"
 import { useState } from "react"
-import { createPortal } from "react-dom"
 
 type HomeFooterProps = {
 
@@ -11,26 +8,11 @@ type HomeFooterProps = {
 export const HomeFooter = ({
 
 }: HomeFooterProps) => {
-  const [isConfigOpen, setIsConfigOpen] = useState<boolean>(false);
 
   return (
     <>
-      {
-        !isConfigOpen && (
-          <footer className='flex-shrink-0 z-10 relative h-20 flex items-center justify-center'>
-            <button  onClick={() => setIsConfigOpen(true)}>
-              <ChevronUp size={40} color={"#fff"}/>
-            </button>
-          </footer>
-        )
-      }
-
-      { isConfigOpen && createPortal(
-        <FooterModal
-          onClose={() => setIsConfigOpen(false)}
-        />,
-        document.body
-      )}
+      <footer className='flex-shrink-0 z-10 relative h-20 flex items-center justify-center'>
+      </footer>
     </>
   )
 }
